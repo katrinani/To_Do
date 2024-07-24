a810adf1f2b0912c211fe8f0de7ef6a75df64f75
katrinani <anisimovakaterina328@gmail.com>
2024-07-24 21:54:32 +0500

    add: логирование

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body, status, Path
 import uuid
+import logging
 from typing import Dict, List
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
@@ -32,14 +33,21 @@ class Task(BaseModel):
     description: str
 
 
+# база данных
 SQLALCHEMY_DATABASE_URL = "sqlite:///././base/sql_app.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# создаем таблицы
 Base.metadata.create_all(bind=engine)
-
-# создаем сессию подключения к бд
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
 app = FastAPI()
+
+# логирование
+logging.basicConfig(
+    level=logging.INFO,
+    filename="logging.log",
+    format="%(asctime)s %(levelname)s %(message)s"
+)
 
 
 @app.post(
@@ -54,23 +62,27 @@ def create_task(
     """
     Создает задачи, валидирует и закидывает в базу данных
     """
-    with SessionLocal() as db:
-        # добавляем в бд
-        task = TasksBase(
-            id=str(uuid.uuid4()),
-            header=header,
-            description=description,
-            done=False,
-            is_deleted=False
-        )
-        print(f"Создана задача {task.id}: {task.header}- {task.description}")
-        db.add(task)
-        db.commit()
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"detail": "Успешное создание"},
-            media_type="application/json"
-        )
+    try:
+        with SessionLocal() as db:
+            # добавляем в бд
+            task = TasksBase(
+                id=str(uuid.uuid4()),
+                header=header,
+                description=description,
+                done=False,
+                is_deleted=False
+            )
+            logging.info(f"Создана задача {task.id}: {task.header}- {task.description}")
+            db.add(task)
+            db.commit()
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Успешное создание"},
+                media_type="application/json"
+            )
+    except Exception:
+        logging.error(Exception)
+
 
 
 @app.get(
@@ -84,26 +96,30 @@ def get_all_tasks() -> JSONResponse:
     """
     Импорт всех актуальных задач из базы данных
     """
-    with SessionLocal() as db:
-        # запрос в бд
-        tasks = db.query(TasksBase).filter(TasksBase.is_deleted == False)
-        print(tasks)
-        # создание ответа
-        all_tasks = {"tasks": []}
-        for task in tasks:
-            all_tasks["tasks"].append({
-                "id": task.id,
-                "header": task.header,
-                "description": task.description,
-                "is_done": task.done
-                }
+    try:
+        with SessionLocal() as db:
+            # запрос в бд
+            tasks = db.query(TasksBase).filter(TasksBase.is_deleted == False)
+            logging.info(f"Получены задачи: {tasks}")
+
+            # создание ответа
+            all_tasks = {"tasks": []}
+            for task in tasks:
+                all_tasks["tasks"].append({
+                    "id": task.id,
+                    "header": task.header,
+                    "description": task.description,
+                    "is_done": task.done
+                    }
+                )
+
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=all_tasks,
+                media_type="application/json"
             )
-        print(all_tasks)
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=all_tasks,
-            media_type="application/json"
-        )
+    except Exception:
+        logging.error(Exception)
 
 
 @app.put(
@@ -119,29 +135,33 @@ def update_task(
     """
     Получение по id задачи и изменение данных этой задачи
     """
-    with SessionLocal() as db:
-        # получение одного объекта по id
-        task = db.get(TasksBase, id)
-        if not task:
-            print("Не найдено задачи с таким id")
+    try:
+        with SessionLocal() as db:
+            # получение одного объекта по id
+            task = db.get(TasksBase, id)
+            if not task:
+                logging.warning(f"Не найдено задачи с id {id}")
+
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"detail": "Не найдено задачи с таким id"},
+                    media_type="application/json"
+                )
+            logging.info(f"Получена задача {task.id}: {task.header}- {task.description}")
+
+            # изменениям значения
+            task.header = header
+            task.description = description
+            db.commit()
+            logging.info(f"Успешное изменение задачи по id {id}")
+
             return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND,
-                content={"detail": "Не найдено задачи с таким id"},
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Успешное изменение"},
                 media_type="application/json"
             )
-        print(f"Получена задача {task.id}: {task.header}- {task.description}")
-
-        # изменениям значения
-        task.header = header
-        task.description = description
-        db.commit()
-        print("Успешное изменение")
-
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"detail": "Успешное изменение"},
-            media_type="application/json"
-        )
+    except Exception:
+        logging.error(Exception)
 
 
 @app.delete(
@@ -153,28 +173,31 @@ def update_task(id: str = Path(pattern=REGEX_UUID)):
     """
     Получение по id задачи и удаление этой задачи
     """
-    with SessionLocal() as db:
-        # получение одного объекта по id
-        task = db.query(TasksBase).filter(TasksBase.id == id).first()
-        if not task:
-            print("Не найдено задачи с таким id")
+    try:
+        with SessionLocal() as db:
+            # получение одного объекта по id
+            task = db.query(TasksBase).filter(TasksBase.id == id).first()
+            if not task:
+                logging.warning(f"Не найдено задачи с id {id}")
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"detail": "Не найдено задачи с таким id"},
+                    media_type="application/json"
+                )
+            logging.info(f"Получена задача {task.id}: {task.header}- {task.description}")
+
+            # удаляем объект
+            task.is_deleted = True
+            db.commit()
+            logging.info(f"Успешное удаление задачи по id {id}")
+
             return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND,
-                content={"detail": "Не найдено задачи с таким id"},
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Успешное удаление"},
                 media_type="application/json"
             )
-        print(f"Получена задача {task.id}: {task.header}- {task.description}")
-
-        # удаляем объект
-        task.is_deleted = True
-        db.commit()
-        print("Успешное удаление")
-
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"detail": "Успешное удаление"},
-            media_type="application/json"
-        )
+    except Exception:
+        logging.error(Exception)
 
 
 @app.put(
@@ -189,34 +212,39 @@ def is_done(
     """
     Получение по id задачи и изменение состояния выполненности этой задачи
     """
-    with SessionLocal() as db:
-        # получение одного объекта по id
-        task = db.get(TasksBase, id)
-        if not task:
-            print("Не найдено задачи с таким id")
+    try:
+        with SessionLocal() as db:
+            # получение одного объекта по id
+            task = db.get(TasksBase, id)
+            if not task:
+                logging.warning(f"Не найдено задачи с id {id}")
+
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"detail": "Не найдено задачи с таким id"},
+                    media_type="application/json"
+                )
+
+            logging.info(f"Получена задача {task.id}: {task.header}- {task.description}")
+
+            if is_done not in [True, False]:
+                logging.warning("Значение не является булевым (True/False)")
+
+                return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"detail": "Значение не является булевым (True/False)"},
+                    media_type="application/json"
+                )
+
+            # изменениям значения
+            task.done = is_done
+            db.commit()
+            logging.info(f"Успешное изменение задачи по id {id}")
+
             return JSONResponse(
-                status_code=status.HTTP_404_NOT_FOUND,
-                content={"detail": "Не найдено задачи с таким id"},
+                status_code=status.HTTP_200_OK,
+                content={"detail": "Успешное изменение"},
                 media_type="application/json"
             )
-        print(f"Получена задача {task.id}: {task.header}- {task.description}")
-
-        if is_done not in [True, False]:
-            print("Значение не является булевым (True/False)")
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={"detail": "Значение не является булевым (True/False)"},
-                media_type="application/json"
-            )
-        # изменениям значения
-        task.done = is_done
-
-        db.commit()
-        print("Успешное изменение")
-
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content={"detail": "Успешное изменение"},
-            media_type="application/json"
-        )
-
+    except Exception:
+        logging.error(Exception)
